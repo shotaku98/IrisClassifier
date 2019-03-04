8940ca69aaf260807c67980927e195d0249ca4ff
shotaku98 <37736680+shotaku98@users.noreply.github.com>
2019-03-04 12:33:29 +0530

    Added dataset and two Classifiers
    classifiers:
    1. tree
    2.Gaussian Naive Bayes

---
+++ IrisModel.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt 
+import pandas as pd 
+from sklearn import tree
+import seaborn as sns
+
+
+df=pd.read_csv("C:/Productivity/Current Work/Iris/Iris.csv")
+#print(df['Species'].value_counts())
+
+sns.pairplot(df,hue='Species',markers='o')
+#plt.show()
+
+X = df.drop(['Id', 'Species'], axis=1)
+y = df['Species']
+
+#Splitting dataset
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5 )
+#print (X_train.shape, y_train.shape)
+#print (X_test.shape, y_test.shape)
+
+clf= tree.DecisionTreeClassifier()
+
+clf=clf.fit(X_train,y_train)
+
+predictions = clf.predict(X_test)
+
+from sklearn import metrics
+print(metrics.accuracy_score(y_test, predictions))
+
+
+#Gaussian Naive Bayes
+
+
+from sklearn.naive_bayes import GaussianNB
+
+gnb=GaussianNB()
+
+gnb=gnb.fit(X_train,y_train)
+
+Gpredictions=gnb.predict(X_test)
+
+print(metrics.accuracy_score(y_test, Gpredictions))
